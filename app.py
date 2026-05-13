94631efa5cb96e524444a3976c694d72cdd0092e
Arshiya <arshiya_ru@yahoo.com>
2026-05-13 09:56:28 +0400

    containerizing the app by creating a Dockerfile

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def download_file(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
